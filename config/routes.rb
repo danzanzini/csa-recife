c4a30fbb6af68ab68ca7fc8ac4ec24aad22a98c4
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-20 20:26:21 -0300

    Adds index action for users through devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-
+  devise_scope :user do
+    get 'users', to: 'users/registrations#index'
+  end
 
   resources :harvests
   resources :offerings do
