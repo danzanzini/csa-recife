1d23fa647194b9ac617080ae6a0dd2ebfad2dd6a
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-20 23:21:57 -0300

    Scaffolds users routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   resources :locations
   root to: 'welcome#index'
 
   devise_for :users
 
-
-
   resources :harvests
   resources :offerings do
     resources :offers, only: [:create, :update, :destroy]
   end
-
 end
