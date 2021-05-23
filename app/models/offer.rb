bba4e5f734f2888bcc33e8af54f59f06e83aad1f
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-23 20:47:03 -0300

    Adds validation to offer fields

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,6 @@ class Offer < ApplicationRecord
   belongs_to :product, optional: true
 
   validates :amount, :product_name, presence: true
+  validates :amount, numericality: { greater_than: 0}
+  validates :product_name, uniqueness: { scope: :offering_id }
 end
