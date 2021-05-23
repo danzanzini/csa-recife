8adb8a356ceb8a93fe910db167c0844e2c1007d3
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-22 21:07:48 -0300

    Translates day of the week name to portuguese

--- app/helpers/offerings_helper.rb
+++ app/helpers/offerings_helper.rb
@@ -1,8 +1,8 @@
 module OfferingsHelper
 
   def seven_days_from_now
-    (Date.today..Date.today+6).map do |day|
-      [day.strftime('%A, %d/%m'), day.strftime('%d-%m')]
+    (Date.today..Date.today+7).map do |day|
+      [I18n.l(day, format: '%A, %d/%m'), day.strftime('%d-%m')]
     end
   end
 end
