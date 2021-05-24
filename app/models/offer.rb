4eebe889a2e20d79b6e8d00ce959e3cb76a8fc88
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-24 19:30:33 -0300

    Improves add offer form

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,7 +2,15 @@ class Offer < ApplicationRecord
   belongs_to :offering
   belongs_to :product, optional: true
 
+  before_validation :normalize_product_name
+
   validates :amount, :product_name, presence: true
   validates :amount, numericality: { greater_than: 0}
   validates :product_name, uniqueness: { scope: :offering_id }
+
+private
+
+  def normalize_product_name
+    self.product_name = product_name&.humanize
+  end
 end
