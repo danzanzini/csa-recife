b5b1c63870f7e03e919dc6d8628272e008985442
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-24 21:30:11 -0300

    Uses time zone to calculate date range

--- app/helpers/offerings_helper.rb
+++ app/helpers/offerings_helper.rb
@@ -1,7 +1,7 @@
 module OfferingsHelper
 
   def seven_days_from_now
-    (Date.today..Date.today+7).map do |day|
+    (Time.zone.today..Time.zone.today + 7).map do |day|
       [I18n.l(day, format: '%A, %d/%m'), day.strftime('%d-%m')]
     end
   end
