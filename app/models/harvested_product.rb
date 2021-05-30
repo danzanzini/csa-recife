fccae07b7507d712ad062edd5df2c72547df2b1f
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-30 19:25:31 -0300

    Adds amount validation to harverst

--- app/models/harvested_product.rb
+++ app/models/harvested_product.rb
@@ -5,7 +5,23 @@ class HarvestedProduct < ApplicationRecord
   validates :amount, numericality: { greater_or_equal_than: 0 }
   # validates :product_name, uniqueness: { scope: :offering_id }
 
+  validate :amount_is_available
+
+  delegate :offering, to: :harvest, allow_nil: true
+
   def self.product_names
     pluck(:product_name)
+  end
+
+private
+  def amount_is_available
+    if offering.available_amount_for(product_name) < amount
+      errors.add(:product_name,
+        message: 'Não há quantidade disponível para este produto'
+      )
+      errors.add(:amount,
+        message: 'Não há quantidade disponível para este produto'
+      )
+    end
   end
 end
