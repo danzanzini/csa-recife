816e363e1c97d03cf350c89a3b2e9d54b415ac5a
Daniel Zanzini <dpzanzini@gmail.com>
2021-05-31 14:56:54 -0300

    Adds total_amount validation to harverst

--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -6,6 +6,8 @@ class Harvest < ApplicationRecord
 
   accepts_nested_attributes_for :harvested_products
 
+  validate :user_amount
+
   def self.new_from(offering)
     new(
       harvested_products: products_from(offering.offers_list),
@@ -17,6 +19,14 @@ private
   def self.products_from(offers_list)
     offers_list.map do |offer_item|
       HarvestedProduct.new(product_name: offer_item, amount: 0)
+    end
+  end
+
+  def user_amount
+    total_amount = harvested_products.reduce(0){ |sum, hp| sum + hp.amount }
+
+    if total_amount > user.items_per_harvest
+      errors.add(:base, "Você escolheu mais itens que o disponível")
     end
   end
 end
