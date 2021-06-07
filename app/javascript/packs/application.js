7eae8a48061f98ef1b97e7f7dabf7fbb5a07be89
Daniel Zanzini <dpzanzini@gmail.com>
2021-06-07 17:17:56 -0300

    Adds increment/decrement buttons to harvests

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,46 @@ import 'vanilla-nested'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+window.addEventListener("turbolinks:load", () => {
+
+  // Code related to increment/decrement buttons behavior
+  function decrement(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value--;
+
+    if(value < 0) { value = 0 }
+    target.value = value;
+  }
+
+  function increment(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value++;
+    target.value = value;
+  }
+
+  const decrementButtons = document.querySelectorAll(
+    `button[data-action="decrement"]`
+  );
+
+  const incrementButtons = document.querySelectorAll(
+    `button[data-action="increment"]`
+  );
+
+  decrementButtons.forEach(btn => {
+    btn.addEventListener("click", decrement);
+  });
+
+  incrementButtons.forEach(btn => {
+    btn.addEventListener("click", increment);
+  });
+});
